2c1de47ff78a21c9934f35628bc5c4cf43205608
Dmitriy-Kulik <0663642996d@gmail.com>
2017-10-30 19:08:57 +0200

    add checks for the existence of a directory

--- functions.php
+++ functions.php
@@ -42,13 +42,14 @@ function uploadsFiles(){
     }
 }
 
-function test(){
-    if(isset($_FILES['upload']) && $_FILES['upload']['type'] == 'image/jpeg' && $_FILES["upload"]["size"] < SIZE) {
-        move_uploaded_file($_FILES['upload']['tmp_name'], 'jpg/' . $_FILES['upload']['name']);
-    }
-}
-
 function showGallery(){
+    if(!file_exists('jpg')){
+        mkdir('jpg/');
+    } else if(!file_exists('pdf')){
+        mkdir('pdf/');
+    } else if(!file_exists('doc')){
+        mkdir('doc/');
+    }
     $arrGall = glob('jpg/*.*');
     $smarty = new Smarty();
     $smarty->setTemplateDir('template');
@@ -60,27 +61,4 @@ function showGallery(){
     $arrNameFileInDirDoc = scandir('doc/');
     $smarty->assign('arrNameFileInDirDoc', $arrNameFileInDirDoc);
     $smarty->display('index.tpl');
-
-    //$arrNameFileInDirJpg = scandir('jpg/');
-/*    echo '<br>Список файлов в папке: ' . realpath('jpg/') . '<br>';
-    foreach ($arrNameFileInDirJpg as $key=>$value) {
-        if ($value != '.' && $value != '..') {
-            echo $key - 1 . ". " . $value . '<br>';
-        }
-    }
-    //$arrNameFileInDirPdf = scandir('pdf/');
-    echo '<br>Список файлов в папке: ' . realpath('pdf/') . '<br>';
-    foreach ($arrNameFileInDirPdf as $key=>$value) {
-        if ($value != '.' && $value != '..') {
-            echo $key - 1 . ". " . $value . '<br>';
-        }
-    }
-    //$arrNameFileInDirDoc = scandir('doc/');
-    echo '<br>Список файлов в папке: ' . realpath('doc/') . '<br>';
-    foreach ($arrNameFileInDirDoc as $key=>$value) {
-        if ($value != '.' && $value != '..') {
-            echo $key - 1 . ". " . $value . '<br>';
-        }
-    }*/
-
 }
